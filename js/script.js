624593579848c85885cb46c15da2e851cd7a4ac5
Abhishek Singhal <abhisinghal112001@gamil.com>
2022-05-10 17:47:00 +0530

    top part

--- js/script.js
+++ js/script.js
@@ -2,15 +2,32 @@ let heading = document.querySelector("#heading")
 let present = document.querySelector("#present")
 let tRight = document.querySelector("#tRight")
 
-setInterval(()=>{
+const i1 = setInterval(()=>{
     present.classList.add("none")
+    window.scrollTo(0, 0);
+    i1off()
 },2500)
-setInterval(()=>{
+function i1off(){
+    clearInterval(i1);
+}
+
+
+const i2 = setInterval(()=>{
     heading.classList.add("head")
+    i2off()
 },5200)
-setInterval(()=>{
+function i2off(){
+    clearInterval(i1);
+}
+
+
+const i3 = setInterval(()=>{
     tRight.classList.add("tRightShow")
+    i3off()
 },6000)
+function i3off(){
+    clearInterval(i1);
+}
 // animation
 
 let sec = document.querySelectorAll("#section")
@@ -35,11 +52,13 @@ function toshow() {
 // check box
 let a=document.getElementById('checkbox')
     a.addEventListener("click",()=>{
-        if(document.getElementById("checkbox").checked == true){
+        if(document.getElementById("checkbox").checked == true){;
             document.getElementById('button').disabled=false
+            document.getElementById('button').classList.remove("fade")
         }
         else{
             document.getElementById('button').disabled=true
+            document.getElementById('button').classList.add("fade")
         }
     })
 
